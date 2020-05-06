fb7c7b7c9b3d838936fc8a43915e1f08a0e30de5
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-05-06 16:51:16 +0300

    Updates

---
+++ precache-manifest.4240fa29731ef4d4ce80f46c8715c649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcbdda2c156dffa48a20cfb7adae82b",
+    "url": "/Calculator-App/index.html"
+  },
+  {
+    "revision": "2a1af18ef8e018eba675",
+    "url": "/Calculator-App/static/css/main.cee3680c.chunk.css"
+  },
+  {
+    "revision": "e56cdaea461880346dec",
+    "url": "/Calculator-App/static/js/2.38091306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-App/static/js/2.38091306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1af18ef8e018eba675",
+    "url": "/Calculator-App/static/js/main.168a9e7f.chunk.js"
+  },
+  {
+    "revision": "9730e89990848e34a43a",
+    "url": "/Calculator-App/static/js/runtime-main.1f4e2f18.js"
+  }
+]);
